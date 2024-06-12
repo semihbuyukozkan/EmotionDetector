4e1c5537fce2954e6ade1ae929f91efccb5c199b
semihbuyukozkan <semihbuyukozkan@gmail.com>
2024-06-12 22:45:03 +0300

    opencv integrated successfully

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+from keras.src.saving.saving_api import load_model
+import os
+import pretrained_model
+
+
+model_path = 'emotion_detection_model.h5'
+if not os.path.exists(model_path):
+    print("Creatıng new model...")
+    pretrained_model.model_creator()
+else:
+    print("Model file found. The model is loading...")
+
+model = load_model('emotion_detection_model.h5')
+
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        face = frame[y:y + h, x:x + w]
+        face_resized = cv2.resize(face, (224, 224))
+        face_array = np.expand_dims(face_resized, axis=0) / 255.0
+
+        predictions = model.predict(face_array)
+        emotion = emotions[np.argmax(predictions)]
+
+        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imshow("Webcam", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ pretrained_model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+
+def model_creator():
+    #model_url = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
+
+    base_model = tf.keras.applications.InceptionV3(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+    #base_model.summary()
+
+    x = layers.GlobalAveragePooling2D()(base_model.output)
+    x = layers.Dense(1024, activation='relu')(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Dense(7, activation='softmax')(x)
+
+    model = Model(inputs= base_model.input, outputs = x)
+
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'] )
+
+    model.summary()
+
+    model.save('emotion_detection_model.h5')
+
+if __name__ == "__main__":
+    model_creator()
